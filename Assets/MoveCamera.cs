a76c5a3e3ddde260d89c8b24612fecea9e79f9c2
CurZedd <Ludvig.strauss@elev.ga.lbs.se>
2022-05-18 10:40:53 +0200

    MOVEMENT GRIS

--- Assets/MoveCamera.cs
+++ Assets/MoveCamera.cs
@@ -2,9 +2,26 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour {
 
-    public Transform player;
+    public float MouseSens = 100f;
+
+    float xRotation = 0f;
+
+    public Transform PlayerBody;
+
+     void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //låser kameran i mitten av spelet -Ludvig 
+    }
 
     void Update() {
-        transform.position = player.transform.position;
+        float MouseX = Input.GetAxis("Mouse X") * MouseSens * Time.deltaTime;
+        float MouseY = Input.GetAxis("Mouse Y") * MouseSens * Time.deltaTime;
+
+        xRotation -= MouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        PlayerBody.Rotate(Vector3.up * MouseX);
     }
 }
